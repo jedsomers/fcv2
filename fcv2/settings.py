4dc0f63c68ac0436165e6941d1b9667ab3f55dfb
Jed Somers <somersjed@gmail.com>
2016-02-12 17:21:36 -0800

    first commit

--- fcv2/settings.py
+++ fcv2/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'fcv2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['fc-v2/fcv2/',],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
